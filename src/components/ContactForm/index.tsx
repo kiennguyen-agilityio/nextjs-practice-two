43d08959bff2550d488bbf2f0e6dbe54f7a148d2
kien.nguyen <kien.nguyen@asnet.com.vn>
2024-08-21 14:55:34 +0700

    feat: build about page and contact page

---
+++ src/components/ContactForm/index.tsx
@@ -0,0 +1,70 @@
+'use client';
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+// Define a type for the form data
+interface FormData {
+  name: string;
+  email: string;
+  phone: string;
+  projectDetails: string;
+}
+
+export default function ContactForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  // Define the type for the onSubmit handler
+  const onSubmit: SubmitHandler<FormData> = (data) => {
+    console.log(data);
+  };
+
+  console.log(errors);
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="mt-12 text-base xs:text-lg sm:text-xl font-medium leading-relaxed font-in"
+    >
+      Hello! My name is{' '}
+      <input
+        type="text"
+        placeholder="your name"
+        {...register('name', { required: true, maxLength: 80 })}
+        className="outline-none border-0 p-0 mx-2 focus:ring-0 placeholder:text-center placeholder:text-lg border-b border-gray
+        focus:border-gray bg-transparent"
+      />
+      and I want to discuss a potential project. You can email me at
+      <input
+        type="email"
+        placeholder="your@email"
+        {...register('email', { required: true })}
+        className="outline-none border-0 p-0 mx-2 focus:ring-0 placeholder:text-center placeholder:text-lg border-b border-gray
+        focus:border-gray bg-transparent"
+      />
+      or reach out to me on
+      <input
+        type="tel"
+        placeholder="your phone"
+        {...register('phone', { required: true })}
+        className="outline-none border-0 p-0 mx-2 focus:ring-0 placeholder:text-center placeholder:text-lg border-b border-gray
+        focus:border-gray bg-transparent"
+      />
+      Here are some details about my project: <br />
+      <textarea
+        {...register('projectDetails', { required: true })}
+        placeholder="My project is about..."
+        rows={3}
+        className="w-full outline-none border-0 p-0 mx-0 focus:ring-0  placeholder:text-lg border-b border-gray
+        focus:border-gray bg-transparent"
+      />
+      <input
+        type="submit"
+        value="send request"
+        className="mt-8 font-medium inline-block capitalize text-lg sm:text-xl py-2 sm:py-3 px-6 sm:px-8 border-2 border-solid border-dark dark:border-light rounded cursor-pointer"
+      />
+    </form>
+  );
+}
